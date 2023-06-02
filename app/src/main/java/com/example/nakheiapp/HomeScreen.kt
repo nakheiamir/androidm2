61c7cf0425185bfa5f73062ea905022cc8eb51e8
Amir <sayawir@gmail.com>
2023-06-02 16:44:57 +0330

    Second commit nakhei

--- app/src/main/java/com/example/nakheiapp/HomeScreen.kt
+++ app/src/main/java/com/example/nakheiapp/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,6 +12,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -23,8 +27,13 @@ fun HomeScreen(navController: NavHostController) {
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(modifier = Modifier
-            .fillMaxWidth(),text = "HomeScreen")
+
+        Text(
+            text = "HomeScreen",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
 
         TextField(
             value = username,
--- app/src/main/java/com/example/nakheiapp/SecondScreen.kt
+++ app/src/main/java/com/example/nakheiapp/SecondScreen.kt
@@ -3,17 +3,32 @@ package com.example.nakheiapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SecondScreen(username: String?, age: Int?){
     Column(modifier = Modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = " khosh amadid '${username}'. sene shoma = ${1402 - age!!} ")
+
+        Text(
+            text = "SecondScreen",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(modifier = Modifier.padding(16.dp),
+            fontSize = 19.sp,
+            text = " khosh amadid '${username}'. sene shoma = ${1402 - age!!} ")
     }
 
     
